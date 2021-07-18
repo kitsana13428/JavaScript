18bb312849dbae6cc458d9bade2edfae44af0258
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-07-18 21:22:12 +0700

    parse string to int

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 document.write("ทดสอบ");
+
+// alert("แจ้งเตือน");
+
+let name; 
+name = "kitsana";
+console.log(name); //เอาตัวแปร name มาแสดงในคอนโซล
+
+//const vat = 7; -> const ทำให้ 7 เป็นค่าคงที่ไม่สามารถเปลี่ยนค่าได้
+///////////////////
+//แปลงค่าตัวอัษรเป้นเลข
+let age = parseInt("20");
+console.log(age);
+console.log(typeof(age));
