6088b663e791332e4be8448544da66ac0ad68ac7
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-09-27 15:35:13 +0700

    Promise
    เรียกใช้งานหลายๆตัว

--- promise.js
+++ promise.js
@@ -1,7 +1,9 @@
 //Promise
 
-const connect = false //เช็คว่าเชื่อมต่อหรือไม่
-const url1 = "cop.raw/file.json"
+const connect = true //เช็คว่าเชื่อมต่อหรือไม่
+const url1 = "cop1.raw/file.json"
+const url2 = "cop2.raw/file.json"
+const url3 = "cop3.raw/file.json"
 
     function downloading(url){
         return new Promise(function(resolve, reject){
@@ -12,12 +14,30 @@ const url1 = "cop.raw/file.json"
                 }else{ 
                     reject("เกิดข้อผิดพลาด")
                 }
-            },3000) //หน่วงเวลา 3 วิ
+            },2000) //หน่วงเวลา 3 วิ
         })
     }
 
+    //การเรียกใช้งาน ปกติ
+/* 
     downloading(url1).then(result => { //เรียกใช้งานฟังก์ชั่น
         console.log(result);
-    }).catch(result => { //ถ้าผิดพลาดให้ทำงาน catch
-        console.log(result);
+    }).catch(err => { //ถ้าผิดพลาดให้ทำงาน catch
+        console.log(err);
+    }).finally(() =>{
+        console.log("จบการทำงาน")
+    })
+*/
+
+    //เรียกใช้งานหลายๆตัว 
+    downloading(url1).then(function(result){ //เรียกใช้งาน url1
+        console.log(result) //แสดงผล
+        return downloading(url2) //ส่งค่าออกไป เรียกใช้งาน url2
+    }).then(function(result){ //ทำงาน url2
+        console.log(result)
+        return downloading(url3) //ส่งค่า url3
+    }).then(function(result){ //ทำงาน url3
+        console.log(result)
+    }).finally(() =>{ //จบการทำงาน
+        console.log("จบการทำงาน") 
     })
